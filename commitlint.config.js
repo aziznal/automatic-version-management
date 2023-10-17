f282f0951dc930ca9c9167656bb8fefda84cd6ae
Aziz <aziznal.dev@gmail.com>
2023-10-17 22:13:07 +0300

    chore: add commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "chore", "docs", "style", "refactor", "perf", "test", "ci"],
+    ],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+    "subject-empty": [2, "never"],
+  },
+};
